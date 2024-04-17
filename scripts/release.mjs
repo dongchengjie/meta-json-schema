084bd5c2e83f04ad5f130770f45f25c872ef0936
dongchengjie <2424014450@qq.com>
2024-04-17 19:49:36 +0800

    feat: support clash-verge Merge

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,21 +1,16 @@
 import bundle from './util/bundle.mjs';
 import { pkg, resolve } from './util/env.mjs';
-import { changelogOutput } from './util/changelog.mjs';
 
 (async () => {
-  // schema文件输入路径
-  const source = resolve(pkg.config.schemaSource);
-  // 最新版本文件输出路径
-  const latestOutputPath = resolve(pkg.config.archiveDir, 'meta-json-schema.json');
-  // 历史版本文件输出路径
-  const version = (process.env['SCHEMA_VERSION'] || process.env['VERSION'] || pkg.version).trim();
-  const historyOutputPath = resolve(pkg.config.archiveDir, version, 'meta-json-schema.json');
-
-  // 打包文件(根据配置决定是否压缩、打印budle成功信息)
-  const optimization = process.env['OPTIMIZATION'] || pkg.config.optimization;
-  await bundle(source, latestOutputPath, optimization, true);
-  await bundle(source, historyOutputPath, optimization, true);
-
-  // 输出changelog
-  changelogOutput(version, resolve(pkg.config.archiveDir, version));
+  // 读取release配置
+  const releases = pkg.config.releases;
+  for (let release of releases) {
+    if (release.enabled) {
+      const source = resolve(release.source);
+      const target = resolve(release.target);
+      const optimization = release.optimization;
+      // 执行bundle
+      await bundle(source, target, optimization, true);
+    }
+  }
 })();
--- scripts/watch.mjs
+++ scripts/watch.mjs
@@ -3,25 +3,26 @@ import path from 'path';
 import bundle from './util/bundle.mjs';
 import { pkg, resolve } from './util/env.mjs';
 
-const source = resolve(pkg.config.schemaSource);
-const dest = resolve('./dest/meta-json-schema.json');
-
 const watchPath = resolve('./src');
 (async () => {
   console.log(`♻️  Watching for: ${watchPath} ...`);
 
-  // 监测目录及子目录的文件变动
-  fs.watch(watchPath, { recursive: true }, async (_type, filename) => {
-    const file = path.resolve(watchPath, filename);
-    if (fs.existsSync(file)) {
-      try {
-        console.log(`Changes in: ${file}`);
+  for (let source of pkg.config.watches) {
+    const target = resolve(`./.temp/${source.substr(source.lastIndexOf('/') + 1)}`);
 
-        // 执行bundle(不压缩、不打印budle成功信息)
-        await bundle(source, dest, false, false);
-      } catch (error) {
-        console.error(`Error watching: ${file}: `, error);
+    // 监测目录及子目录的文件变动
+    fs.watch(watchPath, { recursive: true }, async (_type, filename) => {
+      const file = path.resolve(watchPath, filename);
+      if (fs.existsSync(file)) {
+        try {
+          console.log(`Changes in: ${file}`);
+
+          // 执行bundle
+          await bundle(source, target, false, false);
+        } catch (error) {
+          console.error(`Error watching: ${file}: `, error);
+        }
       }
-    }
-  });
+    });
+  }
 })();
