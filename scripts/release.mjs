13f11f7e0432ac1387c9d1d415ab5a75ca8e18bf
dongchengjie <2424014450@qq.com>
2024-05-06 18:25:39 +0800

    chore: release changelog

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,5 +1,7 @@
-import bundle from './util/bundle.mjs';
-import { pkg, resolve } from './util/env.mjs';
+import bundle from "./util/bundle.mjs";
+import { pkg, resolve } from "./util/env.mjs";
+import { writeToFileSync } from "./util/file.mjs";
+import fs from "fs";
 
 (async () => {
   // 读取release配置
@@ -13,4 +15,15 @@ import { pkg, resolve } from './util/env.mjs';
       await bundle(source, target, optimization, true);
     }
   }
+
+  // 提取CHANGELOG
+  const changelogs = fs.readFileSync(resolve("CHANGELOG.md"), "utf8");
+  writeToFileSync(
+    resolve("CHANGELOG"),
+    changelogs
+      .split("---")
+      .map(log => log.trim())
+      .filter(log => log.startsWith(`## v${pkg.version}`))
+      .filter(Boolean)?.[0] ?? ""
+  );
 })();
